97d434cb0c25d8db234f864d7f7b6212c703b448
Edgardo <edgardo.valderrama@gmail.com>
2018-09-06 18:46:41 -0300

    Subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -46,7 +46,32 @@ let verificaAdmin_Role = (req, res, next) => { //next continua con el programa
 };
 
 
+//=======================
+//Verifica Token para imagen
+//=======================
+
+let verificaTokenImg = (req, res, next) => { //next continua con el programa
+
+
+    let token = req.query.token;
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Error en el Token'
+                }
+            });
+        }
+        req.usuario = decoded.usuario;
+        next();
+    });
+};
+
+
 module.exports = {
     verificaToken,
-    verificaAdmin_Role
+    verificaAdmin_Role,
+    verificaTokenImg
 }
